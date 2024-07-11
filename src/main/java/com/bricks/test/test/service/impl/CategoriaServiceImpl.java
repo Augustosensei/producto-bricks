b09f7742340faad66e8f9db9f34a0b9dd34b0ee3
Augustosensei <augustosensei@gmail.com>
2024-07-11 14:58:08 -0300

    Se agrega limite de cache

--- src/main/java/com/bricks/test/test/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/bricks/test/test/service/impl/CategoriaServiceImpl.java
@@ -1,6 +1,7 @@
 package com.bricks.test.test.service.impl;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
@@ -12,14 +13,21 @@ import com.bricks.test.test.service.ICategoriaService;
 @Service
 public class CategoriaServiceImpl implements ICategoriaService {
 
-	@Autowired
-	private ICategoriaCliente categoriaCliente;
+     @Autowired
+    private ICategoriaCliente categoriaCliente;
+
+    private AtomicInteger solicitudesDiarias = new AtomicInteger(0);
 
     @Override
-    @Cacheable(value = "categoriasCache")
+    @Cacheable(value = "categoriasCache", unless = "#result == null")
     public List<Object> getCategorias() {
+        if (solicitudesDiarias.get() >= 10) {
+            throw new RuntimeException("Se ha alcanzado el límite diario de solicitudes");
+        }
+        
+        // Incrementar el contador de solicitudes
+        solicitudesDiarias.incrementAndGet();
+
         return categoriaCliente.getCategories();
     }
-
-
 }
